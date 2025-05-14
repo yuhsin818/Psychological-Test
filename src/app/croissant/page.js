dee36c7f0aa88985de169534f304626c42972a9d
yuhsin818 <109702047@g.nccu.edu.tw>
2025-05-14 13:00:42 +0800

    514

--- src/app/croissant/page.js
+++ src/app/croissant/page.js
@@ -1,66 +1,54 @@
 'use client';
 
-import StartPage from "@/component/page/StartPage";
-import QuestionPage from "@/component/page/QuestionPage";
-import DisplayResultPage from "@/component/page/DisplayResultPage";
-import ResultPage from "@/component/page/ResultPage";
+import StartPage from '@/component/page/StartPage';
+import QuestionPage from '@/component/page/QuestionPage';
+import DisplayResultPage from '@/component/page/DisplayResultPage';
+import ResultPage from '@/component/page/ResultPage';
 import { useState } from 'react';
+import { usePsyStore } from '@/app/store/store'
 
 export default function Croissant() {
 
-  const [gameState, setGameState] = useState({
-    state: 0, //0: start, 1:question, 2:displayResult, 3:result
-    questionState: 0,
-    totalQuestions: 3,
-    score: 0
-  });
+  const psyState = usePsyStore( (state) => state );
 
   const nextStep = function(){
-    if(gameState.state >= 3) return; //大於3就不執行 return null
     
-    if(gameState.state == 1){
-      
-      //答題階段 須超過總題數才結束
-      if(gameState.questionState < gameState.totalQuestions-1){
-        setGameState({
-          ...gameState, //gameState內的所有屬性
-          questionState: gameState.questionState + 1 //複寫state的值以更新
-        })
-      }else{
-        setGameState({
-          ...gameState, //gameState內的所有屬性
-          state: gameState.state + 1 //複寫state的值以更新
-        })
-      }
+    if(psyState.state >= 3) return;
 
+    if(psyState.state == 1){
+      //答題階段
+      //要超過總題數才能結束答題階段
+
+      if(psyState.questionState < psyState.totalQuestions-1){
+        psyState.updateQuestionState(psyState.questionState + 1);
+      }else{
+        psyState.updateState(psyState.state + 1);
+      }
+      
     }else{
-      setGameState({
-        ...gameState, //gameState內的所有屬性
-        state: gameState.state + 1 //複寫state的值以更新
-      })
+      console.log("next");
+      psyState.updateState(psyState.state + 1);
     }
 
   }
 
   const prevStep = function(){
-    if(gameState.state <= 0) return; //大於3就不執行 return null
-    
-    setGameState({
-      ...gameState, //gameState內的所有屬性
-      state: gameState.state - 1 //複寫state的值以更新
-    })
+    if(psyState.state <= 0) return;
+    console.log("prev");
+    psyState.updateState(psyState.state - 1);
   }
+
 
   return (
     <>
       <div className="w-screen h-screen bg-gray-200 flex justify-center items-center">
-        { gameState.state == 0 && <StartPage/>}
-        { gameState.state == 1 && <QuestionPage questionIndex={gameState.questionState}/>}
-        { gameState.state == 2 && <DisplayResultPage/>}
-        { gameState.state == 3 && <ResultPage/>}
-
-        <div onClick={prevStep}>上一步</div>
-        <div onClick={nextStep}>下一步</div>
+        { psyState.state == 0 && <StartPage nextStep={nextStep} />}
+        { psyState.state == 1 && <QuestionPage nextStep={nextStep} questionIndex={psyState.questionState} />}
+        { psyState.state == 2 && <DisplayResultPage/>}
+        { psyState.state == 3 && <ResultPage/>}
+        
+        {/* <div onClick={prevStep}>上一步</div>
+        <div onClick={nextStep}>下一步</div> */}
       </div>
     </>
   );
--- src/component/page/StartPage.js
+++ src/component/page/StartPage.js
@@ -1,13 +1,30 @@
 'use client';
 
 import MobileFrame from "../layout/MobileFrame";
+import Image from "next/image";
+import titleImg from "@/../public/0.start/title.png";
+import startBtn from "@/../public/0.start/startBtn.png";
+import blurCircle_1 from "@/../public/0.start/blurCircle-1.png";
 
-export default function StartPage() {
+export default function StartPage({nextStep}) {
 
   return (
     <>
       <MobileFrame>
-        StartPage
+        <div className="w-screen h-screen flex justify-center items-center flex-col gap-[60px] font-bold">
+        <Image className="absolute top-0 -translate-y-1/2" src={blurCircle_1} alt=""/>
+          <Image src={titleImg} width={300} alt=""/>
+          <div className="text-[#B65A0F] px-8 text-[14px] flex-wrap leading-loose tracking-wide">
+            有些人天生酥脆，有些人出爐時就塌了。
+            你努力發酵、翻滾、等待出爐，
+            結果還是變成一坨可頌災難。
+            沒關係，這世界不缺完美麵包，
+            缺的是——像你一樣軟爛卻獨特的存在。
+            現在，就來看看你是什麼等級的失控可頌吧。
+          </div>
+          <Image onClick={nextStep} src={startBtn} width={150}  alt=""/>
+          <Image className="absolute bottom-0 translate-y-1/2" src={blurCircle_1} alt=""/>
+        </div>
       </MobileFrame>
     </>
   );
