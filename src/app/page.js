9b5bc721d69a71a35c03f3223d23d3c7095f1d0a
yuhsin818 <109702047@g.nccu.edu.tw>
2025-06-11 13:41:01 +0800

    route change

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,55 @@
-import Image from "next/image";
+'use client';
 
-export default function Home() {
+import StartPage from '@/component/page/StartPage';
+import QuestionPage from '@/component/page/QuestionPage';
+import DisplayResultPage from '@/component/page/DisplayResultPage';
+import ResultPage from '@/component/page/ResultPage';
+import { useState } from 'react';
+import { usePsyStore } from '@/app/store/store'
+
+export default function Croissant() {
+
+  const psyState = usePsyStore( (state) => state );
+
+  const nextStep = function(){
+    
+    if(psyState.state >= 3) return;
+
+    if(psyState.state == 1){
+      //答題階段
+      //要超過總題數才能結束答題階段
+
+      if(psyState.questionState < psyState.totalQuestions-1){
+        psyState.updateQuestionState(psyState.questionState + 1);
+      }else{
+        psyState.updateState(psyState.state + 1);
+      }
+      
+    }else{
+      console.log("next");
+      psyState.updateState(psyState.state + 1);
+    }
+
+  }
+
+  const prevStep = function(){
+    if(psyState.state <= 0) return;
+    console.log("prev");
+    psyState.updateState(psyState.state - 1);
+  }
+
+
   return (
     <>
-      <div>Home</div>
-      <div>Home</div>
+      <div className="w-screen h-screen bg-gray-200 flex justify-center items-center">
+        { psyState.state == 0 && <StartPage nextStep={nextStep} />}
+        { psyState.state == 1 && <QuestionPage nextStep={nextStep} questionIndex={psyState.questionState} />}
+        { psyState.state == 2 && <DisplayResultPage nextStep={nextStep}/>}
+        { psyState.state == 3 && <ResultPage/>}
+        
+        {/* <div onClick={prevStep}>上一步</div>
+        <div onClick={nextStep}>下一步</div> */}
+      </div>
     </>
   );
 }
